3c36b46e48df406326b66ccce0cef17ff6b66a1c
aockert98 <75687390+aockert98@users.noreply.github.com>
2020-12-12 21:39:31 -0500

    Create covid_map.R

---
+++ covid_map.R
@@ -0,0 +1,35 @@
+
+## County data from New York Times' Github Page
+counties <- read.csv("us-counties.csv")
+counties_df <- counties %>%
+  dplyr::filter(state == "Connecticut")
+
+
+library(USAboundariesData)
+library(maps)
+library(USAboundaries)
+
+## Get map
+county_map <- us_counties()
+county_map <- county_map %>%
+  filter(state_abbr == "CT") %>%
+  mutate(fips = as.numeric(geoid))
+
+## Merge with covid data, by FIPS code
+covid_df <- full_join(county_map, counties_df, by = "fips")
+anti_df <- anti_join(county_map, counties_df, by = "fips")
+
+
+## Map 
+map_plot <- covid_df %>%
+  filter(date == "2020-12-10") %>%
+  na.omit() %>%
+  ggplot() +
+  geom_sf(aes(fill = cases)) +
+  geom_sf_text(aes(label = name), size = 3) +
+  scale_fill_gradientn(colors = cols_logged(6)) +
+  labs(title = "COVID-19 throughout Connecticut Counties",
+       fill = "Total Cases") +
+  theme_void() +
+  theme(plot.title = element_text(hjust = 0.5))
+ggplotly(map_plot)
