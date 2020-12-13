abfb093b6266753a1f04940ab86aaa2290211256
aockert98 <75687390+aockert98@users.noreply.github.com>
2020-12-13 17:24:16 -0500

    Update DDS_apps.R

--- DDS_apps.R
+++ DDS_apps.R
@@ -5,6 +5,9 @@ library(tidyr)
 library(ggplot2)
 library(lubridate)
 
+options(tigris_use_cache=TRUE)
+library(tigris)
+
 ## Download Weekly DSS Application Activity from data.ct.gov
 df <- RSocrata::read.socrata("https://data.ct.gov/resource/ymej-83fh.csv")
 dplyr::glimpse(df)
@@ -30,3 +33,12 @@ df2 %>%
   theme(axis.title.x = element_blank(),
         plot.title = element_text(size = 16,
                                   hjust = 0.5))
+
+## Get CT county map data
+towns <- tigris::county_subdivisions(state = "Connecticut") %>%
+  filter(NAMELSAD != "County subdivisions not defined")
+class(towns) #sf
+
+towns %>%
+  ggplot() +
+  geom_sf()
