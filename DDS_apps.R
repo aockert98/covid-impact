fc3be17f6d98fff3b4140c139bac6c2103aa3b95
aockert98 <75687390+aockert98@users.noreply.github.com>
2020-12-13 16:51:39 -0500

    Create DDS_apps.R

---
+++ DDS_apps.R
@@ -0,0 +1,32 @@
+
+library(RSocrata)
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(lubridate)
+
+## Download Weekly DSS Application Activity from data.ct.gov
+df <- RSocrata::read.socrata("https://data.ct.gov/resource/ymej-83fh.csv")
+dplyr::glimpse(df)
+
+df$month <- month(df$week_ending_date, label = TRUE)
+df <- df %>%
+  rename(cash = weekly_applications_received_cash_assistance,
+         medical = weekly_applications_received_medical_assistance,
+         snap = weekly_applications_received_snap,
+         all_apps = weekly_applications_received_all)
+## Wide to long
+df2 <- df %>%
+  pivot_longer(names_to = "Type",
+               values_to = "Applications",
+               cash:snap)
+
+df2 %>%
+  ggplot(aes(week_ending_date, Applications,
+             color = Type)) +
+  geom_line() +
+  labs(title = "Weekly DDS Claims in Connecticut") +
+  theme_minimal() +
+  theme(axis.title.x = element_blank(),
+        plot.title = element_text(size = 16,
+                                  hjust = 0.5))
