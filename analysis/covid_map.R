0f7063d5eb85818cc7af8e68cd98ed3b12ff9da5
mthomas-ketchbrook <52801195+mthomas-ketchbrook@users.noreply.github.com>
2020-12-21 22:28:13 -0500

    Namespacing & moving libs to top of script

--- analysis/covid_map.R
+++ analysis/covid_map.R
@@ -1,33 +1,38 @@
+library(tidyverse)
+library(plotly)
+library(USAboundariesData)
+library(maps)
+library(USAboundaries)
 
 ## County data from New York Times' Github Page
 counties <- read.csv("data/geo/us-counties.csv") %>%
   dplyr::filter(state == "Connecticut")
 
-
-library(USAboundariesData)
-library(maps)
-library(USAboundaries)
-
 ## Get map
-county_map <- us_counties() %>%
-  filter(state_abbr == "CT") %>%
-  mutate(fips = as.numeric(geoid))
+county_map <- USAboundaries::us_counties() %>% 
+  dplyr::filter(state_abbr == "CT") %>%
+  dplyr::mutate(fips = as.numeric(geoid))
 
 ## Merge with covid data, by FIPS code
-covid_df <- full_join(county_map, counties_df, by = "fips")
-anti_df <- anti_join(county_map, counties_df, by = "fips")
+covid_df <- dplyr::full_join(county_map, counties, by = "fips")
+anti_df <- dplyr::anti_join(county_map, counties, by = "fips")
 
 
 ## Map 
 map_plot <- covid_df %>%
-  filter(date == "2020-12-10") %>%
+  dplyr::filter(date == "2020-12-10") %>%
   na.omit() %>%
-  ggplot() +
-  geom_sf(aes(fill = cases)) +
-  geom_sf_text(aes(label = name), size = 3) +
-  scale_fill_gradientn(colors = cols_logged(6)) +
-  labs(title = "COVID-19 throughout Connecticut Counties",
-       fill = "Total Cases") +
-  theme_void() +
-  theme(plot.title = element_text(hjust = 0.5))
-ggplotly(map_plot)
+  ggplot2::ggplot() +
+  ggplot2::geom_sf(ggplot2::aes(fill = cases)) +
+  ggplot2::geom_sf_text(
+    ggplot2::aes(label = name), 
+    size = 3
+  ) +
+  ggplot2::scale_fill_gradientn(colors = cols_logged(6)) +
+  ggplot2::labs(
+    title = "COVID-19 throughout Connecticut Counties",
+    fill = "Total Cases") +
+  ggplot2::theme_void() +
+  ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5))
+
+plotly::ggplotly(map_plot)
