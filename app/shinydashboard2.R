e512595496d19fcb6ec0a0cdc682123cc0be7910
aockert98 <75687390+aockert98@users.noreply.github.com>
2021-01-07 10:32:53 -0500

    Update
    Added Plots, some formatting

--- app/shinydashboard2.R
+++ app/shinydashboard2.R
@@ -28,47 +28,72 @@ sidebar <- dashboardSidebar(
 )
 
 body <- dashboardBody(
-  #fluidRow(
-  # box(plotOutput("plot"))
-  #),
-  box(width = 8,
-      title = "Plot!",
-      plotOutput("plot")),
-  #plotOutput("plot"),
+
   tabItems(
     tabItem(tabName = "Welcome",
-            "Welcome to our project!"),
+            "Welcome to our project!",
+            fluidRow(
+              box(
+                width = 12,
+                title = "About our project"
+              )
+            )),
     tabItem(tabName = "data",
-            "Learn about our data!")),
-  tabBox(
-    title = "Options",
-    tabPanel("Plot1", "Bar"),
-    tabPanel("Plot2", "Second"),
-    tabPanel("Plot3", plotOutput("plot"))))
+            "Learn about our data!",
+            box(
+              width = 12,
+              title = "Interactive Data Table",
+              DT::DTOutput("UI_table")
+            )),
+    tabItem(tabName = "economic",
+            "Explore the Economic Impact of COVID",
+            plotOutput("plot")),
+    tabItem(tabName = "cases",
+            "Explore COVID Cases throughout CT")))
+  
+  #tabBox(
+   # title = "Options",
+    #tabPanel("Plot1", "Bar"),
+    #tabPanel("Plot2", "Second"),
+    #tabPanel("Plot3", plotOutput("plot"))))
 
 
 ui <- dashboardPage(header, sidebar, body, skin = "black")
+
+
 server <- function(input, output) {
   output$plot <- renderPlot({
-    ui_dumb %>%
+    ui_dumb2 %>%
       filter(industry != "Self Employed") %>%
-      ggplot2::ggplot(aes(x = `2013`/1000, xend = `2020`/1000, y = industry, group = industry)) +
+      ggplot2::ggplot(aes(x = year2013/1000, xend = year2020/1000, y = industry, group = industry)) +
       ggalt::geom_dumbbell(color = "darkgray",
                            colour_x = "darkgray",
                            colour_xend="black") +
-      ggplot2::labs(x = "UI Claims Filed",
+      ggplot2::labs(x = "UI Claims Filed (in thousands)",
+                    y = "",
                     title = "The Economic Impact of the Coronavirus Pandemic in Connecticut",
                     subtitle = "Comparing Unemployment Insurance Claims Filed in 2013 vs. 2020", 
                     caption = "Source: (fill in)") +
       ggplot2::theme_minimal() +
-      ggplot2::theme(plot.margin = unit(c(1.5,5,1,1), "cm")) +
+      ggplot2::theme(plot.margin = unit(c(1.5,3,1,0), "cm")) +
       #geom_rect(aes(xmin = 100, xmax = 120, ymin = -Inf, ymax = Inf), fill = "grey") +
-      geom_text(aes(label=paste0(round(delta), "%"), y = industry, x = `2020`/1000), hjust = -0.5, 
+      geom_text(aes(label=paste0(ifelse(delta > 0, "+" , ""), round(delta), "%"), y = industry, x = year2020/1000, color = ifelse(delta > 0, "green", "red")), hjust = -0.5, 
                 size = 3) +
-      ggplot2::geom_text(data = filter(ui_dumb, industry=="Wholesale Trade"),
-                         aes(x = 70, y = industry, label = "Percent Change"), vjust = -1, hjust = 0.5) +
-      ggplot2::scale_y_discrete(expand=c(0.15,0)) +
-      ggplot2::theme(axis.title.y = element_blank())
+      # geom_text(data = filter(ui_dumb, industry=="Wholesale Trade"),
+      #           aes(x = 90, y = industry, label = "Percent Change"), vjust = -1, hjust = 0.5) +
+      scale_y_discrete(expand=c(0.15,0)) + 
+      ggplot2::expand_limits(x = c(-2, 90)) + 
+      ggplot2::theme(legend.position = "none")
+  })
+  output$UI_table <- DT::renderDT({
+    ui_dt %>% 
+      DT::datatable(
+        rownames = FALSE,
+        filter = "top",
+        #colnames = c("Total" = "total_month"),
+        extensions = "Buttons",
+        options = list(dom = "Bfrtip",
+                       buttons = c("csv","excel","pdf")))
   })
 }
 shiny::shinyApp(ui, server)
